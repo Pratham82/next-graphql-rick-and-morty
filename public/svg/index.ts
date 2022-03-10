e1b41c6c96b1e6fecb0e03b606ddcb82f39ccf26
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-03-11 01:48:45 +0530

    feat: added svg icons

---
+++ public/svg/index.ts
@@ -0,0 +1,8 @@
+import Back from './Back'
+import Error from './Error'
+import Loader from './Loader'
+import Moon from './Moon'
+import Sun from './Sun'
+import Close from './Close'
+
+export { Back, Error, Loader, Close, Moon, Sun }
