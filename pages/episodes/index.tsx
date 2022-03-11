a6711130dffdd54512640c1bcb523dfbaa7bf8c9
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-03-11 12:38:08 +0530

    feat: added basic template for remaiing pages

---
+++ pages/episodes/index.tsx
@@ -0,0 +1,13 @@
+import Container from '../../components/Container'
+import Header from '../../components/Header'
+
+export default function Episodes() {
+  return (
+    <Container>
+      <Header />
+      <h2 className="self-center text-center text-5xl text-slate-600">
+        WIP 🚧
+      </h2>
+    </Container>
+  )
+}
