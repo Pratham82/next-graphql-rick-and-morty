3209836799823340d0fb6653634a3b3d5ffc2a4e
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-03-27 15:47:17 +0530

    feat: updated heading

--- pages/locations/index.tsx
+++ pages/locations/index.tsx
@@ -5,9 +5,7 @@ import Header from '../../components/Header'
 export default function Locations() {
   return (
     <Container>
-      <h2 className="self-center pt-4 text-center text-5xl text-slate-600">
-        WIP 🚧 testing out dev branch
-      </h2>
+      <h2 className="self-center pt-4 text-center text-5xl text-slate-600"></h2>
     </Container>
   )
 }
